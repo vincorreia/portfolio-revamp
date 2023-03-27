4f6b3428832a10bbfd47120ef3d9af9bf5c12436
vincorreia <vcorreiaes@gmail.com>
2023-03-27 22:01:48 +0200

    FIX: Projects styles

--- src/components/organisms/Grid.tsx
+++ src/components/organisms/Grid.tsx
@@ -4,7 +4,7 @@ type GridProps = {
 
 export const Grid: React.FC<GridProps> = ({ children }) => {
   return (
-    <div className="grid h-full flex-grow auto-rows-max grid-cols-3 gap-2 py-4 px-2 overflow-x-hidden overflow-y-auto md:grid-cols-4 sm:gap-4 lg:grid-cols-6">
+    <div className="grid h-full flex-grow auto-rows-max gap-6 overflow-y-auto overflow-x-hidden py-4 px-2 sm:grid-cols-2 lg:grid-cols-3">
       {children}
     </div>
   );
--- src/components/organisms/ProjectFigure.tsx
+++ src/components/organisms/ProjectFigure.tsx
@@ -7,12 +7,18 @@ export const ProjectFigure = ({ id, image, title, description }: Project) => {
   return (
     <Link href={`/projects/${id}`}>
       <motion.figure
-        className="cursor-pointer flex flex-col h-full"
+        className="flex h-full cursor-pointer flex-col"
         initial={{ scale: 1 }}
         whileHover={{ scale: 1.05 }}
       >
-        <Image src={image} alt={title} width={300} height={200} className="w-full h-[150px] object-cover"/>
-        <figcaption className="flex flex-col overflow-hidden bg-[var(--code)] bg-opacity-90 p-4 grow">
+        <Image
+          src={image}
+          alt={title}
+          width={300}
+          height={200}
+          className="h-[150px] w-full object-cover object-top"
+        />
+        <figcaption className="flex grow flex-col overflow-hidden bg-[var(--code)] bg-opacity-90 p-4">
           <h3 className="text-xl font-bold text-white">{title}</h3>
           <p className="font-semibold text-zinc-300">{description}</p>
         </figcaption>
